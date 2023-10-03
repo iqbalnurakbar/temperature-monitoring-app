0db073166859692ba02106f0cd2dbe87545401d1
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-10-03 14:45:00 +0700

    menyelesaikan dashboard

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,31 +5,42 @@ import {
   BiUser,
   BiChevronRight,
   BiChevronLeft,
+  BiChevronDown,
   BiLogOut,
 } from 'react-icons/bi';
 import { RiTempColdLine } from 'react-icons/ri';
 
 export default function Sidebar() {
-  const Menus = [
+  const menusBar = [
     { name: 'Home', icon: <BiHomeAlt color="white" /> },
     {
       name: 'Sensor',
       icon: <RiTempColdLine color="white" />,
       icon1: <BiChevronRight color="white" size={12} />,
+      subNav: [
+        {
+          sensorname: 'Sensor 1',
+        },
+        {
+          sensorname: 'Sensor 2',
+        },
+        {
+          sensorname: 'Sensor 3',
+        },
+        {
+          sensorname: 'Sensor 4',
+        },
+      ],
     },
     { name: 'Account', icon: <BiUser color="white" /> },
     { name: 'Log Out', icon: <BiLogOut color="white" />, gap: true },
   ];
-  const MenuSensor = [
-    { sensorname: 'Sensor 1' },
-    { sensorname: 'Sensor 2' },
-    { sensorname: 'Sensor 3' },
-    { sensorname: 'Sensor 4' },
-  ];
+
   const [open, setOpen] = useState(true);
+  const [subNavOpen, setSubNavOpen] = useState(false);
 
   return (
-    <div
+    <nav
       className={`${
         open ? 'w-40' : 'w-12'
       } duration-300 h-screen bg-teal-600 relative p-2`}
@@ -54,29 +65,48 @@ export default function Sidebar() {
           </h1>
         </div>
         <ul className="">
-          {' '}
-          {Menus.map((val, index) => {
+          {menusBar.map((val, index) => {
             return (
-              <li
-                key={index}
-                className={`flex items-center gap-x-3 rounded-md hover:bg-teal-700 cursor-pointer p-2 ${
-                  val.gap ? 'mt-9' : 'mt-2'
-                } `}
-              >
-                <div>{val.icon}</div>
-                <span
-                  className={`text-xs text-white mr-4 ${
-                    !open && 'hidden'
-                  } origin-left duration-200`}
+              <>
+                <li
+                  key={index}
+                  className={`flex items-center gap-x-3 rounded-md hover:bg-teal-700 cursor-pointer p-2 ${
+                    val.gap ? 'mt-9' : 'mt-2'
+                  }`}
                 >
-                  {val.name}
-                </span>
-                <div>{val.icon1}</div>
-              </li>
+                  <div>{val.icon}</div>
+                  <span
+                    className={`text-xs text-white mr-4 ${
+                      !open && 'hidden'
+                    } origin-left duration-200`}
+                  >
+                    {val.name}
+                  </span>
+                  {val.subNav && (
+                    <BiChevronDown
+                      color="white"
+                      onClick={() => setSubNavOpen(!subNavOpen)}
+                      className={`${subNavOpen && 'rotate-180'} duration-300`}
+                    />
+                  )}
+                </li>
+                {val.subNav && subNavOpen && open && (
+                  <ul>
+                    {val.subNav.map((subNavItem, idx) => (
+                      <li
+                        key={idx}
+                        className="flex items-center gap-x-3 rounded-md hover:bg-teal-700 cursor-pointer p-2 text-white text-xs"
+                      >
+                        {subNavItem.sensorname}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </>
             );
           })}
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
---
+++ src/components/Sidebar/sidebarData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BiHomeAlt } from 'react-icons/bi';
+
+const qwerty = [
+  {
+    name: 'Home',
+    icon: <BiHomeAlt />,
+  },
+];
+
+export default sidebarData;
