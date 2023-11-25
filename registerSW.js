2347391239cb794534538345e6a43a44baae9e31
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-11-25 15:12:23 +0700

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/temperature-monitoring-app/sw.js', { scope: '/temperature-monitoring-app/' })})}
