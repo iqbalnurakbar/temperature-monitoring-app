7d778195beeed78ec5a699cff276d3563328d03f
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-11-26 16:46:31 +0700

    mistakes again

---
+++ app/build/generated/source/buildConfig/release/app/netlify/cd_monitoring_suhu/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.netlify.cd_monitoring_suhu.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.netlify.cd_monitoring_suhu.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
