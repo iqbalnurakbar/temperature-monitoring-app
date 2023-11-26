b9435bcaf6a04f20667aa717227ebef87911fb14
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-11-26 23:56:50 +0700

    adjust another assetlinks.json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "22271febdc61e0ae248cd93e3ec01f59"
   }, {
     "url": "index.html",
-    "revision": "0.09gp26ctkh8"
+    "revision": "0.spiovs2499"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
