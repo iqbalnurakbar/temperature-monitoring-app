625f134b8df68cbfdfb7c01e18216312bc0a414a
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-12-08 16:34:22 +0700

    mengaktifkan API untuk mesin 4-6

--- src/data/apiConfigurations.js
+++ src/data/apiConfigurations.js
@@ -4,14 +4,9 @@ export const apiConfigurations1 = (startDate, endDate, startTime, endTime) => [
     fieldIndices: [1, 2, 4, 5],
     timeIndices: [3, 3, 6, 6],
   },
-  // {
-  //   url: `https://api.thingspeak.com/channels/2344351/feeds.json?timezone=Asia%2FJakarta&start=${startDate}%20${startTime}:00&end=${endDate}%20${endTime}:59`,
-  //   fieldIndices: [1, 2, 4, 5],
-  //   timeIndices: [3, 3, 6, 6],
-  // },
-  // {
-  //   url: `https://api.thingspeak.com/channels/2347341/feeds.json?timezone=Asia%2FJakarta&start=${startDate}%20${startTime}:00&end=${endDate}%20${endTime}:59`,
-  //   fieldIndices: [1, 2, 4, 5],
-  //   timeIndices: [3, 3, 6, 6],
-  // },
+  {
+    url: `https://api.thingspeak.com/channels/2347341/feeds.json?timezone=Asia%2FJakarta&start=${startDate}%20${startTime}:00&end=${endDate}%20${endTime}:59`,
+    fieldIndices: [1, 2, 4],
+    timeIndices: [3, 3, 6],
+  },
 ];
