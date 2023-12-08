c7c5449859ba62a49de2ba935d4c80b06a140478
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-12-08 15:53:47 +0700

    mengaktifkan channel thingspeak field 2, 4, 5

--- src/data/apiConfigurations.js
+++ src/data/apiConfigurations.js
@@ -1,8 +1,8 @@
 export const apiConfigurations1 = (startDate, endDate, startTime, endTime) => [
   {
     url: `https://api.thingspeak.com/channels/2342296/feeds.json?timezone=Asia%2FJakarta&start=${startDate}%20${startTime}:00&end=${endDate}%20${endTime}:59`,
-    fieldIndices: [1],
-    timeIndices: [3],
+    fieldIndices: [1, 2, 4, 5],
+    timeIndices: [3, 3, 6, 6],
   },
   // {
   //   url: `https://api.thingspeak.com/channels/2344351/feeds.json?timezone=Asia%2FJakarta&start=${startDate}%20${startTime}:00&end=${endDate}%20${endTime}:59`,
