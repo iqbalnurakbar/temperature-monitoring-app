c4019a4aee2d266a706e2a553286ec1e3bc3ee40
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2023-12-18 18:26:19 +0700

    Mengubah API Thingspeak

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "22271febdc61e0ae248cd93e3ec01f59"
   }, {
     "url": "index.html",
-    "revision": "0.8rhl31bh69o"
+    "revision": "0.p0hre93nb28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
